01310c6d143c071b90648d83fcc6f9ec0bb78cae
takaki <cheap_trick_magic@yahoo.co.jp>
2021-03-22 16:06:36 +0900

    CustomerLogモデルの作成 CustomerLogsテーブルの作成 CusotemrLogFactoryの作成

---
+++ server/database/factories/CustomerLogFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CustomerLog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerLogFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = CustomerLog::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
