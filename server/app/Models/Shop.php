8f7ccbd816ae9f77edfa5c0ffd2e3d513254120f
takaki <cheap_trick_magic@yahoo.co.jp>
2021-03-22 15:07:46 +0900

    Shopモデルとshopsテーブルの作成

---
+++ server/app/Models/Shop.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    use HasFactory;
+
+    const SHOP_ID_TOKYO = 1;
+    const SHOP_ID_NAGOYA = 2;
+    const SHOP_ID_OSAKA = 3;
+    protected $guarded = [];
+}
