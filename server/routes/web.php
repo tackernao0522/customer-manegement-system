14a64001bf845893917165ca4b83e800b5040a46
takaki <cheap_trick_magic@yahoo.co.jp>
2021-03-25 18:18:58 +0900

    bladeとルーティングの学習

--- server/routes/web.php
+++ server/routes/web.php
@@ -20,3 +20,34 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/test', function () {
+    return 'Hello World';
+});
+
+Route::get('/test2', function () {
+    return '
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello HTML</title>
+    </head>
+    <body>
+        <h1 style="color: hotpink;">Hello HTML</h1>
+    </body>
+    </html>
+    ';
+});
+
+Route::get('/test3', function () {
+    return view('hello');
+});
+
+Route::get('/page_a', function () {
+    return view('page_a');
+});
+
+Route::get('/page_b', function () {
+    return view('page_b');
+});
